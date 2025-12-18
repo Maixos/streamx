f479c653eb036b6cc0958850f865c6bd76093a19
MincoX <903444601@qq.com>
2025-12-18 16:11:49 +0800

    Update

---
+++ src/jetson/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+// -----------------------------------------------------------
+// Main Entry
+// -----------------------------------------------------------
+int main(int argc, char* argv[]) {
+    std::cout << "StreamX Demo Starting..." << std::endl;
+
+    // TODO
+    return 0;
+}
